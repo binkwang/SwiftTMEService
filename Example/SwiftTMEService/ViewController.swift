c5c503d94e3a86a676487aedb911202f85fdf3fc
Bink Wang <fxmmc@sina.com>
2018-09-07 15:10:31 +1200

    Initial commit

---
+++ Example/SwiftTMEService/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftTMEService
+//
+//  Created by hanmbink on 09/07/2018.
+//  Copyright (c) 2018 hanmbink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
